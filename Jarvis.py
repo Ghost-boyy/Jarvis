033f1e53c4cac8ffdf4e3fdbc12dded585baf2a7
Jhonatha Oliver <jhonatha222@gmail.com>
2021-11-04 12:42:09 -0300

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,14 +1,21 @@
 import pyttsx3
-
+import datetime
 texto_fala = pyttsx3.init()
 
 def falar(audio):
     rate = texto_fala.getProperty('rate')
-    texto_fala.setProperty("rate",170)
+    texto_fala.setProperty("rate",170)# Alterar a velocidade da fala
     voices = texto_fala.getProperty('voices')
     texto_fala.setProperty('voice',voices[0].id)
     texto_fala.say(audio)
     texto_fala.runAndWait()
 
-falar("olá a todos. o meu nome é Live.")
+    Tempo = datetime.datetime.now().strftime("%I:%M:%S")
+    falar("Agora são: ")
+    falar(Tempo)
+
+def date():
+    ano = datetime.datetime.now().year
+    mes = datetime.datetime.now().month
+    dia = datetime.datetime.now().day
 
