7523f49dbe4f251c64c940a3cfa46933f8b20b54
Jhonatha Oliver <jhonatha222@gmail.com>
2021-11-11 13:02:58 -0300

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,25 +1,31 @@
-from typing_extensions import Annotated
+
 import pyttsx3
 import datetime
+
 texto_fala = pyttsx3.init()
 
 def falar(audio):
     rate = texto_fala.getProperty('rate')
     texto_fala.setProperty("rate",170)# Alterar a velocidade da fala
     voices = texto_fala.getProperty('voices')
-    texto_fala.setProperty('voice',voices[0].id)
+    texto_fala.setProperty('voice',voices[1].id)
     texto_fala.say(audio)
     texto_fala.runAndWait()
 
 def tempo():
     Tempo = texto_fala.getProperty("%I:%M")
-    
+    falar("Agora são: ")
     falar(Tempo)
     
 
-def data():
-    ano = str(datetime.datetime.now().year)
-    mes = str(datetime.datetime.now().month)
-    dia = str(datetime.datetime.now().day)
-    falar("a data atual é: ")
-    falar(dia)
+#def data():
+   # ano = str(datetime.datetime.now().year)
+   # mes = str(datetime.datetime.now().month)
+    #dia = str(datetime.datetime.now().day)
+    #falar("a data atual é: ")
+    #falar(dia)
+    #falar("de" + mes)
+    #falar("de"+ ano)
+
+tempo()
+#data()
